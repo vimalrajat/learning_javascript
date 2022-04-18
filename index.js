391955662d16ac04e1f7779d84d9ef3e46e1326d
vimalrajat <vimalrajat@gmail.com>
2022-04-18 15:07:20 +0530

    First time file creation for testing

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// Hello world
+// console.log("Hello World");
+
+// console.log("I am learning Javascript")
+
+// Functions
+
+// traditional function definitions
+// sum = function(a,b){
+//     return a+b
+// }
+// console.log(sum(4,5))
+
+// use of function expression
+// sum = function(a,b){
+//     return a+b
+// }
+
+// var funExp = sum(10,3)
+// console.log(funExp)
+
+// use of anonymous function
+// funExp = function(a,b){
+//     return a+b
+// }
+
+// console.log(funExp(3,2));
+// console.log(funExp(3,3));
+
+// use of strings literal
+//           Writing a table
+
+tableFunction = function(){
+    var tableOf = 6;
+    
+    for(i=1; i<=10; i++){
+        console.log(`${tableOf} * ${i} = ${tableOf * i}`);
+    }
+}
+
+tableFunction();
